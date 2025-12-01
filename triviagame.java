83b4a9fd489fe36bd0e6aa7b30f6f189fd21ff56
Joey Laurin <jlaurin@emich.edu>
2025-12-01 11:48:14 -0500

    Initial commit - project setup

---
+++ triviagame.java
@@ -0,0 +1,12 @@
+// This class calls methods from the question class to run the trivia game.
+/* Author: Joey Laurin
+Date: 11/23/2025
+Purpose: To run the trivia game by invoking the main method of the question class.
+ */ 
+public class triviagame {
+    public static void main(String[] args) {
+        question trivia = new question();
+        trivia.main(args);
+        
+    }
+}

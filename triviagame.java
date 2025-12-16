14cfd325777e30325873979d4baba9ce63d440ae
jlaurin1 <jlaurin@emich.edu>
2025-12-15 22:46:53 -0500

    Update triviagame.java

--- triviagame.java
+++ triviagame.java
@@ -6,7 +6,7 @@ Purpose: To run the trivia game by invoking the main method of the question clas
 public class triviagame {
     public static void main(String[] args) {
         question trivia = new question(); // create an instance of the question class
-        trivia.main(args); // call the main method of the question class to start the game
-        
+        trivia.playGame(); // call the playGame method from the question class to run the game
+        trivia.getScore(); // call the getScore method from the question class to display the final score
     }
 }
